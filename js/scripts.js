3085c3162eb85289678c5a7907d533ec4d786ff5
rebecca allen and zachary baum <rebecca@>
2016-08-18 10:27:39 -0400

    add the ability to turn all single digits into roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,26 @@ $("#converter").submit(function(event) {
 
   // var digit = parseInt(digits)
 
-  for (i=1; i<=(digits[digits.length - 1]); i++) {
-    if (parseInt(digits[digits.length - 1]) <= 3) {
-      $('.results').append("I")
+
+  if (parseInt(digits[digits.length - 1]) <= 3) {
+    for (i=1; i<=(digits[digits.length - 1]); i++) {
+    $('.results').append("I") }
+
+  } else if (parseInt(digits[digits.length - 1]) === 4) {
+    $('.results').append("IV")
+
+  } else if (parseInt(digits[digits.length - 1]) >= 5) {
+    if (parseInt(digits[digits.length - 1]) === 5) {
+      $('.results').append("V")
     }
-  }
-});
+
+    else if ((parseInt(digits[digits.length - 1]) >= 5) && (parseInt(digits[digits.length - 1]) != 9)) {
+      $('.results').append("V")
+      for (i=6; i<=(digits[digits.length - 1]); i++) {
+        $('.results').append("I") }
+      }
+      else {
+        $('.results').append("IX")
+      }
+    }
+  })
