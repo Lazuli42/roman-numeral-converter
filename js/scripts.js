bbf2f64eef7ddb00db419631fc4424d1bb044f22
rebecca allen and zachary baum <rebecca@>
2016-08-18 10:46:00 -0400

    add the ability to convert 2 digit numbers to roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,31 @@ $("#converter").submit(function(event) {
 
   // var digit = parseInt(digits)
 
+  if (digits.length === 2){
+    if (parseInt(digits[digits.length - 2]) <= 3) {
+      for (i=1; i<=(digits[digits.length - 2]); i++) {
+      $('.results').append("X") }
+
+    }
+    else if (parseInt(digits[digits.length - 2]) === 4) {
+      $('.results').append("XL")
+    }
+    else if (parseInt(digits[digits.length - 2]) >= 5) {
+      if (parseInt(digits[digits.length - 2]) === 5) {
+        $('.results').append("L")
+      }
+
+      else if ((parseInt(digits[digits.length - 2]) >= 5) && (parseInt(digits[digits.length - 2]) != 9)) {
+        $('.results').append("L")
+        for (i=6; i<=(digits[digits.length - 2]); i++) {
+          $('.results').append("X")
+        }
+      }
+      else {
+          $('.results').append("XC")
+      }
+    }
+  }
 
   if (parseInt(digits[digits.length - 1]) <= 3) {
     for (i=1; i<=(digits[digits.length - 1]); i++) {
